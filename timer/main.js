aad0e74dc26e64c1571a0d668e8837c0026deb38
Sean Findlay <seanfindlay3@gmail.com>
2018-08-05 20:40:54 +0200

    Changed some files

--- timer/main.js
+++ timer/main.js
@@ -8,7 +8,7 @@ var seconds;
 
 function update(){
 	rawTime += 0.01
-	cleanTime = clean(rawTime.toString());
+	cleanTime = clean(rawTime.toString(), 1);
 	//console.log(cleanTime);
 	output.innerHTML = cleanTime;
 }
@@ -26,21 +26,48 @@ while (isOn){
 }
 
 function stop(){
+	final = Math.round(rawTime * 100) / 100;
+	var result;
+	var mins = Math.floor(final / 60);
+	var secs = final - Math.floor(final / 60) * 60;
+	var lastpart = (((Math.round(final) * 100) / 100) % 1) + secs;
+	if(final > 60){
+		result = mins + ":" + Math.round(lastpart * 100) / 100;
+	}else if(final >= 10){
+		result = "0.00";
+	}else{
+		result = final;
+	}
+	output.innerHTML = result;
 	clearInterval(interval);
 	isOn = false;
 	startBtn.innerHTML = "start";
 }
 
-function clean(time){
+function clean(time, points){
 	timeSplit = time.split("");
 	var num = Number(timeSplit.join().replace(/,/g, ""));
-	var secs = Math.round(num * 1) / 1;
+	var secs = Math.floor(num);
 	var mins = Math.floor(secs / 60);
 	secs = secs - mins * 60;
-	if(mins > 0){
-		return mins + ":" + (Math.round(((Math.round(num * 100) / 100) % 1) * 100) / 100 + secs);
-	}else{
-		return Math.round(num * 100) / 100;
+	if(points == 1){
+		if(mins > 0){
+			//var lastpart = ((Math.round(num * 100) / 100) % 1) + secs;
+			return mins + ":" + secs;//Math.round(lastpart * 100) / 100;
+		}else if(mins == 10){
+			stop();
+		}else{
+			return Math.floor(num);
+		}
+	}else if(points == 2){
+		if(mins > 0){
+			var lastpart = ((Math.round(num * 100) / 100) % 1) + secs;
+			return mins + ":" + Math.round(lastpart * 100) / 100;
+		}else if(mins == 10){
+			stop();
+		}else{
+			return Math.round(num * 100) / 100;
+		}
 	}
 }
 
@@ -52,14 +79,12 @@ startBtn.addEventListener('click', function(){
 	}
 });
 
-/*document.body.onkeyup = function(e){
+document.body.onkeyup = function(e){
 	if(e.keyCode == 32){
 		if (isOn){
 			stop();
-			console.log('stopping');
 		}else{
 			start();
-			console.log('starting');
 		}
 	}
-}*/
+}
